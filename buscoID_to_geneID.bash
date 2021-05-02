27d58098290e28b5873627bfb50d6247a07b17a9
Viraj Muthye <vrmuthye@iastate.edu>
2021-05-02 14:07:16 -0500

    Update buscoID_to_geneID.bash

--- buscoID_to_geneID.bash
+++ buscoID_to_geneID.bash
@@ -1,11 +1,14 @@
-#loops through a list of BUSCO ids and then gets the uniport ID from OrthoDB using the OrthoDB API
-
+#BUSCO does not provide mapping of BUSCO ID and Uniprot ID
+#This script loops through a list of BUSCO ids and then gets the uniport ID from OrthoDB using the OrthoDB API
+#Usage ./buscoID_to_geneID.bash filename
+#filename is a list of BUSCO ids with one on each line
 
 while read p;do
 	wget "https://www.orthodb.org/tab?id=$p&species=9606" -O "$p".tab
 done < $1
 cat *.tab | awk -F "\t" '{print $7}' | grep -v "pub_gene_id" > "$1"_uniprot_ids
 rm *.tab
+
 
 
 
